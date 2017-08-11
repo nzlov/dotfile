93df0f22e9e10b65aedb23006a1a091b4718fd5f
nzlov <wfhtqp@gmail.com>
2017-08-11 22:32:49 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 git clone https://github.com/nzlov/dotfile ~/.nzlovdotfile
+rm ~/.vimrc
 ln -s ~/.nzlovdotfile/vimrc ~/.vimrc
+rm ~/.tmux.conf
 ln -s ~/.nzlovdotfile/tmux.conf ~/.tmux.conf
-cp ~/.nzlovdotfile/tmux.conf.local ~/tmux.conf.local
+rm ~/.tmux.conf.local
+cp ~/.nzlovdotfile/tmux.conf.local ~/.tmux.conf.local
