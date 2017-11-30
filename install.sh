48718da2792df8875e70320b0dbcbae0c85d08ac
wfhtqp <1006954237@qq.com>
2017-11-30 16:34:29 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 git clone https://github.com/nzlov/dotfile ~/.nzlovdotfile
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 rm ~/.zshrc
-ln -s ~/.nzlovdotfile/zshrc ~/.zshrc
+ln -s ~/.nzlovdotfile/zshrc.v2 ~/.zshrc
 #source ~/.zshrc
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm ~/.vimrc
